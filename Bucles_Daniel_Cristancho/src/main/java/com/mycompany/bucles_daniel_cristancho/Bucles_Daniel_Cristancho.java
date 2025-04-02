808cc8a20612c802726d65a35444962ed9ea936c
DanielCA21 <dasancristal91@gmail.com>
2025-04-02 08:48:25 -0500

    Add files via upload

---
+++ Bucles_Daniel_Cristancho/src/main/java/com/mycompany/bucles_daniel_cristancho/Bucles_Daniel_Cristancho.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.bucles_daniel_cristancho;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Bucles_Daniel_Cristancho {
+
+    public static void main(String[] args) {
+        int contador = 15;
+        
+        while (contador > 0) {
+        System.out.println(contador);
+        
+        contador = contador -1;
+        
+    }
+    }
+}
