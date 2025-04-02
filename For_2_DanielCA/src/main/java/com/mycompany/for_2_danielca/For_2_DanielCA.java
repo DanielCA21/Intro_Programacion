212e4634ba1c30adca71e7f0c92f7f2fad9582e2
DanielCA21 <dasancristal91@gmail.com>
2025-04-02 08:10:18 -0500

    Add files via upload

---
+++ For_2_DanielCA/src/main/java/com/mycompany/for_2_danielca/For_2_DanielCA.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.for_2_danielca;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class For_2_DanielCA {
+
+    public static void main(String[] args) {
+        String productos[] = {"cafe", "chocolate", "te", "gaseosa", "agua", "yogurt"};
+        
+        for (int i=0; i < productos.length; i++) {
+            System.out.println(productos[i]);
+            
+        }
+        
+        
+    }
+}
