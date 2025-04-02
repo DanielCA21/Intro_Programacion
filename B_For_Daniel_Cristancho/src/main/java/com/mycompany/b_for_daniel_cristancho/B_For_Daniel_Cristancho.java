5e74ff0e35f17fe823d1ef70b403aa9f7f5c43c2
DanielCA21 <dasancristal91@gmail.com>
2025-04-02 08:49:00 -0500

    Add files via upload

---
+++ B_For_Daniel_Cristancho/src/main/java/com/mycompany/b_for_daniel_cristancho/B_For_Daniel_Cristancho.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.b_for_daniel_cristancho;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class B_For_Daniel_Cristancho {
+
+    public static void main(String[] args) {
+        
+        int contador = 15;
+        for (i = 10; contador >= 0; i++) {
+        
+        System.out.println(contador);
+    }
+    }
+}
